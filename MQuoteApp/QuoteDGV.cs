08cf61bf9642f3b92ec5437756024b814aba1964
元斗 秋山 <akiyama19851985@gmail.com>
2023-04-05 00:20:42 +0900

    QuoteDVG追加

--- MQuoteApp/QuoteDGV.cs
+++ MQuoteApp/QuoteDGV.cs
@@ -10,14 +10,11 @@ namespace MQuoteApp
 
     public class QuoteDGV : DataGridView
     {
-        private DataGridView dataGridView1;
         private int _draggedIndex;
 
         public QuoteDGV()
         {
-
-            AllowDrop = true; 
-
+            AllowDrop = true;
         }
 
         protected override void OnMouseDown(MouseEventArgs e)
@@ -27,10 +24,16 @@ namespace MQuoteApp
             if (e.Button == MouseButtons.Left && e.Clicks == 1)
             {
                 _draggedIndex = this.HitTest(e.X, e.Y).RowIndex;
-                if (_draggedIndex >= 0)
-                {
-                    this.DoDragDrop(this.Rows[_draggedIndex], DragDropEffects.Move);
-                }
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (e.Button == MouseButtons.Left && _draggedIndex >= 0)
+            {
+                this.DoDragDrop(this.Rows[_draggedIndex], DragDropEffects.Move);
             }
         }
 
@@ -38,6 +41,7 @@ namespace MQuoteApp
         {
             base.OnDragOver(e);
 
+            this.ClearSelection();
             e.Effect = DragDropEffects.None;
             if (e.Data.GetDataPresent(typeof(DataGridViewRow)))
             {
@@ -45,7 +49,6 @@ namespace MQuoteApp
                 if (targetIndex >= 0 && targetIndex != _draggedIndex)
                 {
                     e.Effect = DragDropEffects.Move;
-                    this.Rows[_draggedIndex].Selected = false;
                     this.Rows[targetIndex].Selected = true;
                 }
             }
@@ -72,17 +75,17 @@ namespace MQuoteApp
         {
             if (e.Control && e.KeyCode == Keys.C) // Ctrl + Cでコピー
             {
-                CopySelectedRowsToClipboard();
+                CopySelectedRowsToClipboard(this);
             }
             else if (e.Control && e.KeyCode == Keys.V) // Ctrl + Vで貼り付け
             {
                 // 貼り付け処理を実装
-                PasteRows();
+                PasteRows(this);
             }
             else if (e.Control && e.KeyCode == Keys.X) // Ctrl + Xで切り取り
             {
                 // 切り取り処理を実装
-                CutSelectedRows();
+                CutSelectedRows(this);
             }
             else if (e.Control && e.KeyCode == Keys.Enter) // Enterキーで次の列に移動
             {
@@ -105,105 +108,47 @@ namespace MQuoteApp
             }
         }
 
-        private void CopySelectedRowsToClipboard()
+        private void CopySelectedRowsToClipboard(DataGridView dgv)
         {
-            // 選択された行を取得
-            DataGridViewSelectedRowCollection selectedRows = dataGridView1.SelectedRows;
-
-            // 行データを文字列に連結してクリップボードにコピー
             StringBuilder sb = new StringBuilder();
-            foreach (DataGridViewRow row in selectedRows)
+
+            foreach (DataGridViewRow row in dgv.SelectedRows)
             {
                 foreach (DataGridViewCell cell in row.Cells)
                 {
                     sb.Append(cell.Value);
-                    sb.Append("\t"); // タブ区切りで列を区切る
+                    sb.Append("\t");
                 }
-                sb.Remove(sb.Length - 1, 1); // 最後のタブを削除
-                sb.AppendLine(); // 行を区切る
+
+                sb.Remove(sb.Length - 1, 1); // Remove the last tab
+                sb.AppendLine();
             }
+
             Clipboard.SetText(sb.ToString());
         }
 
-        private void CutSelectedRows()
+        private void CutSelectedRows(DataGridView dgv)
         {
-            if (dataGridView1.SelectedRows.Count == 0)
-            {
-                return;
-            }
+            CopySelectedRowsToClipboard(dgv);
 
-            // 切り取り先のコレクションを作成する
-            var cutCollection = new List<DataGridViewRow>();
-            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            foreach (DataGridViewRow row in dgv.SelectedRows)
             {
-                cutCollection.Add(row);
-            }
-
-            // コレクションから削除する
-            foreach (var row in cutCollection)
-            {
-                dataGridView1.Rows.Remove(row);
+                dgv.Rows.Remove(row);
             }
         }
 
-        private void PasteRows()
+        private void PasteRows(DataGridView dgv)
         {
-            // クリップボードからデータを取得する
-            string clipboardData = Clipboard.GetText();
+            string[] rows = Clipboard.GetText().Split('\n');
 
-            // クリップボードのデータが空でない場合、行を貼り付ける
-            if (!string.IsNullOrEmpty(clipboardData))
+            foreach (string row in rows)
             {
-                // 改行で分割して、行ごとのデータを取得する
-                string[] rowData = clipboardData.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-                // 選択した行の最後の行番号を取得する
-                int lastRowIndex = dataGridView1.SelectedRows[dataGridView1.SelectedRows.Count - 1].Index;
-
-                // 選択した行の最後の行の次の行に新しい行を挿入する
-                dataGridView1.Rows.Insert(lastRowIndex + 1);
-
-                // 挿入した行にクリップボードから取得したデータを貼り付ける
-                for (int i = 0; i < rowData.Length; i++)
+                if (row.Length > 0)
                 {
-                    // タブで分割して、セルごとのデータを取得する
-                    string[] cellData = rowData[i].Split('\t');
-
-                    // セルの数が列の数よりも大きい場合は、余分なセルを切り捨てる
-                    int cellCount = Math.Min(cellData.Length, dataGridView1.ColumnCount);
-
-                    // セルのデータを設定する
-                    for (int j = 0; j < cellCount; j++)
-                    {
-                        dataGridView1.Rows[lastRowIndex + i + 1].Cells[j].Value = cellData[j];
-                    }
+                    dgv.Rows.Add(row.Split('\t'));
                 }
             }
         }
-
-        private void InitializeComponent()
-        {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // QuoteDGV
-            // 
-            this.RowTemplate.Height = 24;
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
-            this.ResumeLayout(false);
-
-        }
     }
+
 }
