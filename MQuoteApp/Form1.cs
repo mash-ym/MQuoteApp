7faafd0e27d929a590b85a0ed2a6e1d5ccaa69a5
元斗 秋山 <akiyama19851985@gmail.com>
2023-04-04 23:37:04 +0900

    QuoteDVG追加

--- MQuoteApp/Form1.cs
+++ MQuoteApp/Form1.cs
@@ -1,20 +1,130 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SQLite;
 using System.Windows.Forms;
 
 namespace MQuoteApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        // データベースファイルのパス
+        private const string dbFilePath = @"C:\path\to\database.db";
+
+        public MainForm()
         {
             InitializeComponent();
         }
+
+        // 新規プロジェクト作成ボタンがクリックされたときの処理
+        private void NewProjectButton_Click(object sender, EventArgs e)
+        {
+            // 新しいプロジェクトを作成し、TreeViewに追加
+            var newProject = new Project("新しいプロジェクト");
+            var projectNode = new TreeNode(newProject.Name);
+            projectNode.Tag = newProject;
+            treeView1.Nodes.Add(projectNode);
+
+            // DataGridViewに空のデータを表示
+            dataGridView1.DataSource = new DataTable();
+        }
+
+        // ノードが選択されたときの処理
+        private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // 選択されたノードがProjectノードである場合
+            if (e.Node.Tag is Project)
+            {
+                // DataGridViewにそのProjectの見積データを表示
+                var project = (Project)e.Node.Tag;
+                ShowQuoteData(project.Quotes);
+            }
+            // 選択されたノードがQuoteノードである場合
+            else if (e.Node.Tag is QuoteData)
+            {
+                // DataGridViewにその見積データを表示
+                var quote = (QuoteData)e.Node.Tag;
+                ShowQuoteData(new QuoteData[] { quote });
+            }
+        }
+
+        // DataGridViewに見積データを表示するメソッド
+        private void ShowQuoteData(QuoteData[] quotes)
+        {
+            // 接続文字列
+            string connectionString = $"Data Source={dbFilePath}";
+
+            // データベース接続オブジェクトを作成
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                // 見積データを取得するSQL文
+                string sql = "SELECT * FROM Quotes WHERE ";
+                for (int i = 0; i < quotes.Length; i++)
+                {
+                    if (i != 0)
+                    {
+                        sql += " OR ";
+                    }
+                    sql += $"(ProjectName = '{quotes[i].ProjectName}' AND SubcontractorName = '{quotes[i].SubcontractorName}')";
+                }
+                sql += ";";
+
+                // SELECT文を実行してデータリーダーを取得
+                using (var command = new SQLiteCommand(sql, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        // データリーダーから取得したデータをDataGridViewに表示
+                        var dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+
+        // DataGridViewのセルがダブルクリックされたときの処理
+        private void DataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        { // 選択された行の見積データを取得
+            var selectedRow = dataGridView1.Rows[e.RowIndex];
+            var projectName = selectedRow.Cells["ProjectName"].Value.ToString();
+            var subcontractorName = selectedRow.Cells["SubcontractorName"].Value.ToString();
+            var amount = selectedRow.Cells["Amount"].Value.ToString();
+            var date = selectedRow.Cells["Date"].Value.ToString();
+            // 見積データを編集するダイアログを表示
+            using (var dialog = new EditQuoteDialog())
+            {
+                dialog.ProjectName = projectName;
+                dialog.SubcontractorName = subcontractorName;
+                dialog.Amount = amount;
+                dialog.Date = date;
+
+                var result = dialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    // 編集された見積データを保存
+                    var editedQuote = new QuoteData
+                    {
+                        ProjectName = dialog.ProjectName,
+                        SubcontractorName = dialog.SubcontractorName,
+                        Amount = dialog.Amount,
+                        Date = dialog.Date
+                    };
+
+                    quoteManager.UpdateQuoteData(selectedRow.Index, editedQuote);
+
+                    // DataGridViewに表示されている見積データを更新
+                    dataGridView1.Rows[selectedRow.Index].Cells["ProjectName"].Value = editedQuote.ProjectName;
+                    dataGridView1.Rows[selectedRow.Index].Cells["SubcontractorName"].Value = editedQuote.SubcontractorName;
+                    dataGridView1.Rows[selectedRow.Index].Cells["Amount"].Value = editedQuote.Amount;
+                    dataGridView1.Rows[selectedRow.Index].Cells["Date"].Value = editedQuote.Date;
+                }
+            }
+        }
+
     }
 }
---
+++ MQuoteApp/QuoteDGV.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MQuoteApp
+{
+
+    public class QuoteDGV : DataGridView
+    {
+        private DataGridView dataGridView1;
+        private int _draggedIndex;
+
+        public QuoteDGV()
+        {
+
+            AllowDrop = true; 
+
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if (e.Button == MouseButtons.Left && e.Clicks == 1)
+            {
+                _draggedIndex = this.HitTest(e.X, e.Y).RowIndex;
+                if (_draggedIndex >= 0)
+                {
+                    this.DoDragDrop(this.Rows[_draggedIndex], DragDropEffects.Move);
+                }
+            }
+        }
+
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            base.OnDragOver(e);
+
+            e.Effect = DragDropEffects.None;
+            if (e.Data.GetDataPresent(typeof(DataGridViewRow)))
+            {
+                var targetIndex = this.HitTest(e.X, e.Y).RowIndex;
+                if (targetIndex >= 0 && targetIndex != _draggedIndex)
+                {
+                    e.Effect = DragDropEffects.Move;
+                    this.Rows[_draggedIndex].Selected = false;
+                    this.Rows[targetIndex].Selected = true;
+                }
+            }
+        }
+
+        protected override void OnDragDrop(DragEventArgs e)
+        {
+            base.OnDragDrop(e);
+
+            if (e.Data.GetDataPresent(typeof(DataGridViewRow)))
+            {
+                var targetIndex = this.HitTest(e.X, e.Y).RowIndex;
+                if (targetIndex >= 0 && targetIndex != _draggedIndex)
+                {
+                    var draggedRow = (DataGridViewRow)e.Data.GetData(typeof(DataGridViewRow));
+                    this.Rows.RemoveAt(_draggedIndex);
+                    this.Rows.Insert(targetIndex, draggedRow);
+                    this.CurrentCell = this[0, targetIndex];
+                }
+            }
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C) // Ctrl + Cでコピー
+            {
+                CopySelectedRowsToClipboard();
+            }
+            else if (e.Control && e.KeyCode == Keys.V) // Ctrl + Vで貼り付け
+            {
+                // 貼り付け処理を実装
+                PasteRows();
+            }
+            else if (e.Control && e.KeyCode == Keys.X) // Ctrl + Xで切り取り
+            {
+                // 切り取り処理を実装
+                CutSelectedRows();
+            }
+            else if (e.Control && e.KeyCode == Keys.Enter) // Enterキーで次の列に移動
+            {
+                // Enterキー処理を実装
+                e.Handled = true;
+                int currentColumnIndex = CurrentCell.ColumnIndex;
+                int currentRowIndex = CurrentCell.RowIndex;
+                if (currentColumnIndex == Columns.Count - 1)
+                {
+                    if (currentRowIndex < Rows.Count - 1)
+                    {
+                        CurrentCell = Rows[currentRowIndex + 1].Cells[0];
+                    }
+                }
+                else
+                {
+                    CurrentCell = Rows[currentRowIndex].Cells[currentColumnIndex + 1];
+
+                }
+            }
+        }
+
+        private void CopySelectedRowsToClipboard()
+        {
+            // 選択された行を取得
+            DataGridViewSelectedRowCollection selectedRows = dataGridView1.SelectedRows;
+
+            // 行データを文字列に連結してクリップボードにコピー
+            StringBuilder sb = new StringBuilder();
+            foreach (DataGridViewRow row in selectedRows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    sb.Append(cell.Value);
+                    sb.Append("\t"); // タブ区切りで列を区切る
+                }
+                sb.Remove(sb.Length - 1, 1); // 最後のタブを削除
+                sb.AppendLine(); // 行を区切る
+            }
+            Clipboard.SetText(sb.ToString());
+        }
+
+        private void CutSelectedRows()
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                return;
+            }
+
+            // 切り取り先のコレクションを作成する
+            var cutCollection = new List<DataGridViewRow>();
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                cutCollection.Add(row);
+            }
+
+            // コレクションから削除する
+            foreach (var row in cutCollection)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
+        }
+
+        private void PasteRows()
+        {
+            // クリップボードからデータを取得する
+            string clipboardData = Clipboard.GetText();
+
+            // クリップボードのデータが空でない場合、行を貼り付ける
+            if (!string.IsNullOrEmpty(clipboardData))
+            {
+                // 改行で分割して、行ごとのデータを取得する
+                string[] rowData = clipboardData.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                // 選択した行の最後の行番号を取得する
+                int lastRowIndex = dataGridView1.SelectedRows[dataGridView1.SelectedRows.Count - 1].Index;
+
+                // 選択した行の最後の行の次の行に新しい行を挿入する
+                dataGridView1.Rows.Insert(lastRowIndex + 1);
+
+                // 挿入した行にクリップボードから取得したデータを貼り付ける
+                for (int i = 0; i < rowData.Length; i++)
+                {
+                    // タブで分割して、セルごとのデータを取得する
+                    string[] cellData = rowData[i].Split('\t');
+
+                    // セルの数が列の数よりも大きい場合は、余分なセルを切り捨てる
+                    int cellCount = Math.Min(cellData.Length, dataGridView1.ColumnCount);
+
+                    // セルのデータを設定する
+                    for (int j = 0; j < cellCount; j++)
+                    {
+                        dataGridView1.Rows[lastRowIndex + i + 1].Cells[j].Value = cellData[j];
+                    }
+                }
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // QuoteDGV
+            // 
+            this.RowTemplate.Height = 24;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
