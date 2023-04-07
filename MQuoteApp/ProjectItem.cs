fcd943a9d65822ce13d2d38c1b7dfc4e043410f7
元斗 秋山 <akiyama19851985@gmail.com>
2023-04-07 23:24:35 +0900

    ProjectItem.cs生成

---
+++ MQuoteApp/ProjectItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQuoteApp
+{
+    public abstract class ProjectItem
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<ProjectItem> SubItems { get; set; }
+        public int WorkingDays => GetWorkingDays();
+
+        protected ProjectItem(string name, DateTime startDate, DateTime endDate)
+        {
+            Name = name;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        protected int GetWorkingDays()
+        {
+            int days = 0;
+            DateTime currentDay = StartDate;
+            while (currentDay <= EndDate)
+            {
+                if (currentDay.DayOfWeek != DayOfWeek.Saturday && currentDay.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    days++;
+                }
+                currentDay = currentDay.AddDays(1);
+            }
+            return days;
+        }
+
+        public virtual decimal GetTotalCost()
+        {
+            return 0m;
+        }
+    }
+}
