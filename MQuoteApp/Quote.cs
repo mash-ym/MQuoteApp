11fe36dfd9557a76714714d21e49b69f2966dbad
元斗 秋山 <akiyama19851985@gmail.com>
2023-04-07 23:48:09 +0900

    Quote.cs生成

---
+++ MQuoteApp/Quote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQuoteApp
+{
+    public class Quote
+    {
+        public decimal TotalAmount { get; set; }
+        public decimal TotalEstimatedAmount { get; set; }
+
+        public decimal CalculateAmount(IEnumerable<EstimateItem> items)
+        {
+            decimal totalAmount = 0;
+            decimal totalEstimatedAmount = 0;
+
+            foreach (var item in items)
+            {
+                totalAmount += item.GetTotalCost();
+                totalEstimatedAmount += item.EstimatedAmount;
+            }
+
+            TotalAmount = totalAmount;
+            TotalEstimatedAmount = totalEstimatedAmount;
+
+            return totalAmount;
+        }
+    }
+
+}
