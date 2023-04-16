c35d30b3204db4206586ef37efacaa8142beb900
元斗 秋山 <akiyama19851985@gmail.com>
2023-04-16 20:47:37 +0900

    EstimateDependencyGraph機能追加

--- MQuoteApp/EstimateDependencyGraph.cs
+++ MQuoteApp/EstimateDependencyGraph.cs
@@ -7,11 +7,13 @@ namespace MQuoteApp
     {
         private List<EstimateItem> _nodes;
         private Dictionary<EstimateItem, List<EstimateItem>> _edges;
+        private Dictionary<EstimateItem, List<EstimateItem>> _dependencies;
 
         public EstimateDependencyGraph()
         {
             _nodes = new List<EstimateItem>();
             _edges = new Dictionary<EstimateItem, List<EstimateItem>>();
+            _dependencies = new Dictionary<EstimateItem, List<EstimateItem>>();
         }
 
         public void AddNode(EstimateItem node)
@@ -38,21 +40,94 @@ namespace MQuoteApp
                 _edges[from].Add(to);
             }
         }
+        public void AddDependency(EstimateItem item, List<EstimateItem> dependentItems)
+        {
+            if (!_dependencies.ContainsKey(item))
+            {
+                _dependencies[item] = new List<EstimateItem>();
+            }
+
+            foreach (var dependentItem in dependentItems)
+            {
+                if (!_dependencies.ContainsKey(dependentItem))
+                {
+                    _dependencies[dependentItem] = new List<EstimateItem>();
+                }
+
+                _dependencies[item].Add(dependentItem);
+            }
+        }
+        public Dictionary<EstimateItem, List<EstimateItem>> GetDependencies()
+        {
+            return _dependencies;
+        }
 
         public bool HasCircularDependency()
         {
-            HashSet<EstimateItem> visited = new HashSet<EstimateItem>();
-            HashSet<EstimateItem> recursionStack = new HashSet<EstimateItem>();
+            var visited = new HashSet<EstimateItem>();
+            var inProgress = new HashSet<EstimateItem>();
 
-            foreach (EstimateItem node in _nodes)
+            foreach (var item in _dependencies.Keys)
             {
-                if (HasCircularDependencyRecursive(node, visited, recursionStack))
+                if (HasCircularDependency(item, visited, inProgress))
                 {
                     return true;
                 }
             }
 
             return false;
+        }
+        private bool HasCircularDependency(EstimateItem item, HashSet<EstimateItem> visited, HashSet<EstimateItem> inProgress)
+        {
+            if (inProgress.Contains(item))
+            {
+                return true;
+            }
+
+            if (visited.Contains(item))
+            {
+                return false;
+            }
+
+            visited.Add(item);
+            inProgress.Add(item);
+
+            foreach (var dependentItem in _dependencies[item])
+            {
+                if (HasCircularDependency(dependentItem, visited, inProgress))
+                {
+                    return true;
+                }
+            }
+
+            inProgress.Remove(item);
+
+            return false;
+        }
+        public DateTime CalculateFinishDate()
+        {
+            var visited = new HashSet<EstimateItem>();
+            var finishDates = new Dictionary<EstimateItem, DateTime>();
+
+            foreach (var item in _dependencies.Keys)
+            {
+                if (!visited.Contains(item))
+                {
+                    Visit(item, visited, finishDates);
+                }
+            }
+
+            DateTime finishDate = DateTime.MinValue;
+
+            foreach (var date in finishDates.Values)
+            {
+                if (date > finishDate)
+                {
+                    finishDate = date;
+                }
+            }
+
+            return finishDate;
         }
 
         private bool HasCircularDependencyRecursive(EstimateItem node, HashSet<EstimateItem> visited, HashSet<EstimateItem> recursionStack)
@@ -104,15 +179,36 @@ namespace MQuoteApp
             }
         }
 
-        public void Visit(Action<EstimateItem> visitor)
+        private void Visit(EstimateItem item, HashSet<EstimateItem> visited, Dictionary<EstimateItem, DateTime> finishDates)
         {
-            HashSet<EstimateItem> visited = new HashSet<EstimateItem>();
-            foreach (EstimateItem node in _nodes)
+            visited.Add(item);
+
+            DateTime maxFinishDate = DateTime.MinValue;
+
+            foreach (var dependentItem in _dependencies[item])
             {
-                VisitRecursive(node, visited, visitor);
+                if (!visited.Contains(dependentItem))
+                {
+                    Visit(dependentItem, visited, finishDates);
+                }
+
+                if (finishDates.ContainsKey(dependentItem) && finishDates[dependentItem] > maxFinishDate)
+                {
+                    maxFinishDate = finishDates[dependentItem];
+                }
+            }
+
+            finishDates[item] = item.CalculateEndDate(maxFinishDate);
+        }
+        public void RemoveDependency(EstimateItem item)
+        {
+            _dependencies.Remove(item);
+
+            foreach (var dependentItems in _dependencies.Values)
+            {
+                dependentItems.Remove(item);
             }
         }
-
         private void VisitRecursive(EstimateItem node, HashSet<EstimateItem> visited, Action<EstimateItem> visitor)
         {
             if (!visited.Contains(node))
